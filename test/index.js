39fa78bf05c12d9b052f2228e123507ae6d7cd50
Addy Osmani <addyosmani@gmail.com>
2014-06-14 20:29:52 +0100

    Update tests.

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ var oust = require('../index');
 
 it('should return an array of stylesheet link hrefs', function (done) {
 	fs.readFile('test/sample/index.html', function read(err, data) {
-		var links = oust(data);
+		var links = oust(data, 'stylesheets');
 		assert(links.length == 2);
 		assert(links[0] == 'bower_components/bootstrap/dist/css/bootstrap.css');
 		assert(links[1] == 'styles/main.css');
@@ -14,7 +14,7 @@ it('should return an array of stylesheet link hrefs', function (done) {
 });
 
 it('should return an array of refs when passed a HTML string', function (done) {
-	var links = oust('<html><link rel="stylesheet" href="styles/main.css"></html>');
+	var links = oust('<html><link rel="stylesheet" href="styles/main.css"></html>', 'stylesheets');
 	assert(links.length == 1);
 	assert(links[0] == 'styles/main.css');
 	done();
@@ -22,7 +22,7 @@ it('should return an array of refs when passed a HTML string', function (done) {
 
 it('should return an array of script srcs', function (done) {
 	fs.readFile('test/sample/index.html', function read(err, data) {
-		var links = oust(data, 'script');
+		var links = oust(data, 'scripts');
 		assert(links.length == 1);
 		assert(links[0] == 'scripts/main.js');
 		done();
@@ -31,7 +31,7 @@ it('should return an array of script srcs', function (done) {
 
 it('should return an array of HTML imports', function (done) {
 	fs.readFile('test/imports.html', function read(err, data) {
-		var links = oust(data, 'import');
+		var links = oust(data, 'imports');
 		assert(links.length == 3);
 		assert(links[0] == '../polymer/polymer.html');
 		assert(links[1] == '../core-ajax/core-ajax.html');
@@ -48,4 +48,17 @@ it('should fail if no valid source is specified', function (done) {
 		 }
 		done();
 	}
+});
+
+it('should fail if no valid type is specified', function (done) {
+		fs.readFile('test/imports.html', function read(err, data) {
+			try {
+				var links = oust(data);
+			} catch (e) {
+				 if(/ReferenceError: type is not defined/.test(err)) {
+				 	return true;
+				 }
+				done();				
+			}
+		});
 });
