addyosmani/oust

commit 987de3611b0998bedb612abdba158d54b6459d8c

require node 12 & fix linter

cli.js
@@ -32,11 +32,9 @@ if (argv.h || argv.help || argv._.length === 0) {
 
 const [file, type] = argv._;
 
-fs.readFile(file, (error, data) => {
-    if (error) {
+fs.promises.readFile(file)
+    .then(data => console.log(oust(data, type).join('\n')))
+    .catch(error => {
         console.error('Error opening file:', error.message);
         process.exit(1);
-    }
-
-    console.log(oust(data, type).join('\n'));
-});
+    });

.github/workflows/test.yml
@@ -3,29 +3,29 @@ name: Tests
 on: [push, pull_request]
 
 env:
-  FORCE_COLOR: 2
+    FORCE_COLOR: 2
 
 jobs:
-  run:
-    name: Node ${{ matrix.node }}
-    runs-on: ubuntu-latest
+    run:
+        name: Node ${{ matrix.node }}
+        runs-on: ubuntu-latest
 
-    strategy:
-      fail-fast: false
-      matrix:
-        node: [8, 10, 12, 14, 16]
+        strategy:
+            fail-fast: false
+            matrix:
+                node: [12, 14, 16]
 
-    steps:
-      - name: Clone repository
-        uses: actions/checkout@v2
+        steps:
+            - name: Clone repository
+              uses: actions/checkout@v2
 
-      - name: Set up Node.js
-        uses: actions/setup-node@v2
-        with:
-          node-version: ${{ matrix.node }}
+            - name: Set up Node.js
+              uses: actions/setup-node@v2
+              with:
+                  node-version: ${{ matrix.node }}
 
-      - name: Install npm dependencies
-        run: npm ci
+            - name: Install npm dependencies
+              run: npm ci
 
-      - name: Run tests
-        run: npm test
+            - name: Run tests
+              run: npm test

commit 4d04baf30a5398a23fb4906ecf37e6ed865f1173

cli.js: fix readFile missing encoding broken since 987de36
Previously, the data was returned as buffer

cli.js
@@ -33,7 +33,7 @@ if (argv.h || argv.help || argv._.length === 0) {
 
 const [file, type] = argv._;
 
-fs.promises.readFile(file)
+fs.promises.readFile(file, 'utf8')
   .then(data => console.log(oust(data, type).join('\n')))
   .catch(error => {
     console.error(`Error opening file: ${error.message}`);